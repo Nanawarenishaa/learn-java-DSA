cf288d08004f701d80abd1316e6acf9a9ce5af6f
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2024-06-09 20:02:57 +0530

    Add files via upload

--- factorialNo.java
+++ factorialNo.java
@@ -1,17 +1,34 @@
 import java.util.Scanner;
 
-class factorialNo {
+public class factorialNo {
     public static void main (String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter a number:");
         int num = scanner.nextInt();
 
-        int factorial = 1 ;
+        long factorial = 1 ;
         for(int i = 1; i<=num ; i ++){
             factorial *= i;
 
         }
-       System.out.println("factorial of " + num + "factorial");
+       System.out.println("factorial of " + num + " is " + factorial);
        scanner.close();
+    }
+}
+  
+class factorialNumber{
+    public static void main(String[] args){
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("enter a number : ");
+       int num = scanner.nextInt();
+       long factorial= factorial(num);
+       System.out.println("factorial of " + num + " : " + factorial);
+      scanner.close();
+    }
+    public static long factorial(int num){
+        if (num == 0){
+            return 1;
+        }
+        return num* factorial(num-1);
     }
 }
