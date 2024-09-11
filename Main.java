807f1ba732455b585e8238d9249d7b20005b706c
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2024-09-11 08:31:38 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,23 +1,30 @@
-public class Main {
-    public static void main(String[] args) {
-      System.out.println("Hello World");
-    }
-  }
+class car {
+	
+	String model;
+	int year;
+    String color;
+   
+	public car(String Model , int Year ){
+         this.model = Model;
+         this.year =Year;
+
+	}
+
+	public car(String Model , int Year, String Color){
+         this.model = Model;
+         this.year =Year;
+         this.color= Color;
+       
+
+	}
 
 
- 
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter first number: ");
-        int num1 = scanner.nextInt();
-
-        System.out.print("Enter second number: ");
-        int num2 = scanner.nextInt();
-
-        int sum = num1 + num2;
-        System.out.println("Sum: " + sum);
-    }
 }
+ public class Main{
+  public static void main(String args[]){
+     car Car1=new car("toyota" , 2004);
+     System.out.println("first car detail:" + Car1.model + " " + Car1.year );
+      car Car2=new car("toyota2" , 2005 , "brown");
+     System.out.println("first car detail:" + Car2.model + " " + Car2.year +" " +Car2.color );
+  }
+ }
